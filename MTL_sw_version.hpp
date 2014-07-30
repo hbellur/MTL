a6879e6239b37fccf66b84f476cba77b57f5144c
hbellur <hbellur@gmail.com>
2014-07-30 16:07:12 -0400

    Create MTL_sw_version.hpp

---
+++ MTL_sw_version.hpp
@@ -0,0 +1,6 @@
+/// @file MTL_sw_version.hpp
+
+/// Reusable Multi Threaded Library software version.  The values must be
+/// updated manually prior to officially building the software.
+/// NOTE: This version uses boost release version 1.55.0 
+#define MTL_SW_VERSION "2.0"
